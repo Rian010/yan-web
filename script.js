5d381558fe2ae02439a8655536fd3736c873c3aa
Rian010 <mhmmdriann@gmail.com>
2023-02-13 04:46:27 +0800

    menghapus slide show

--- script.js
+++ script.js
@@ -24,12 +24,3 @@ window.onscroll = function() {
     }
 };
 
-const slideshow = document.querySelector(".slideshow");
-      const images = Array.from(slideshow.children);
-      let currentImageIndex = 0;
-
-      setInterval(() => {
-        images[currentImageIndex].style.display = "none";
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].style.display = "block";
-      }, 3000);
